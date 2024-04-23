33ffcbed19b1719c853e9f270fb578cb5e316461
Kerry Chang <kerry30338@gmail.com>
2024-04-23 11:21:41 +0800

    Add method definitions.

--- CmdHandler.java
+++ CmdHandler.java
@@ -1,12 +1,35 @@
+import java.util.*;
+
 public class CmdHandler {
-    private Library library;
-    public void addUser(type: String, name: String, booksLimit: String)
-    + checkout(staff_id: String, book_ids: String[], user_id: String): void
-    + returnBook(staff_id: String, book_id: String): void
-    + addBook(staff_id: String, author_id: String, subject_id: String): void
-    + removeBook(staff_id: String, book_id: String): void
-    + getBooksByAuthor(staff_id: String, author_id: String): List<Book>
-    + getBooksBySubject(staff_id: String, subject_id: String): List<Book>
-    + getBooksByBorrower(user_id: String, user_id: String): List<Book>
-    + findLastBorrower(staff_id: String, book_id: String): Borrower
+    private Library library = new Liberary();
+
+    public void addUser(String type, String name, String booksLimit) {
+        return;
+    }
+
+    public void checkout(String staff_id, String[] book_ids, String user_id) {
+        return;
+    }
+
+    public void returnBook(String staff_id, String book_id) {
+        return;
+    }
+    public void addBook(String staff_id, String author_id, String subject_id) {
+        return;
+    }
+    public void removeBook(String staff_id, String book_id) {
+        return;
+    }
+    public List<Book> getBooksByAuthor(String staff_id, String author_id) {
+        return new ArrayList<Book>();
+    }
+    public List<Book> getBooksBySubject(String staff_id, String subject_id) {
+        return new ArrayList<Book>();
+    }
+    public List<Book> getBooksByBorrower(String user_id, String user_id) {
+        return new ArrayList<Book>();
+    }
+    public Borrower findLastBorrower(String staff_id, String book_id) {
+        return new Borrower();
+    }
 }
