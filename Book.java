5dc59334bc52ec3a15f58107410e0f444129df92
r12944053 <swim1357924681o@gmail.com>
2024-04-23 12:15:04 +0800

    Author、Subject Book OK  Libarary writing

--- Book.java
+++ Book.java
@@ -1,2 +1,63 @@
-public class Book {
+public class Book implements Comparable<Book>{
+    private String name;
+    private boolean status;
+    private Author author;
+    private Subject subject;
+    private User borrower;
+
+    private static int book_count = 0;
+
+    public Book(Author author, Subject subject) {
+        this.name = String.valueOf(book_count);
+        book_count++;
+        status = true;
+        this.author = author;
+        this.subject = subject;
+        borrower = null;
+    }
+
+    public static int getBook_count() {
+        return book_count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public User getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        return this.name.compareTo(other.getName());
+    }
 }
--- Library.java
+++ Library.java
@@ -1,4 +1,66 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Library {
+    private final Map<String, User> users;
+    private final Map<String, Book> books;
+    private final Map<String, Author> authors;
+    private Map<String, Subject> subjects;
+
+    private static int book_count = 0;
+
+    public Library(){
+        users = new HashMap<>();
+        books = new HashMap<>();
+        authors = new HashMap<>();
+        subjects = new HashMap<>();
+    }
+
+    public void addUser(User user){
+        users.put(user.getName(), user);
+    }
+
+    public User getUser(String userName){
+        return users.get(userName);
+    }
+
+    public List<User> getUsers(){
+        return new ArrayList<>(users.values());
+    }
+
+    public void addBook(Book book){
+        Library.book_count = Book.getBook_count();
+        books.put(Integer.toString(Library.book_count), book);
+    }
+
+    public List<Book> getBooks(){
+        return new ArrayList<>(books.values());
+    }
+
+    public Book removeBook(String bookName){
+        Book removedBook = books.get(bookName);
+        if(removedBook == null){
+            return null;
+        }
+        books.remove(bookName);
+        return removedBook;
+    }
+
+    public void addAuthor(Author author){
+        authors.put(author.getName(), author);
+    }
+
+    public Author getAuthor(String author_id){
+        return authors.get(author_id);
+    }
+
+    public void addSubject(Author author){
+        subjects.put(author.getName(), author);
+    }
+
+    public Author getSubject(String subject_id){
+        return subjects.get(subject_id);
+    }
 }
